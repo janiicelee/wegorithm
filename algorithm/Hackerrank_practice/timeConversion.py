cd02638ea02efe8d4c39926506b5bfd4ec27807d
Yerin Lee <meekukin@gmail.com>
2020-03-24 21:13:19 +0900

    time

--- algorithm/Hackerrank_practice/timeConversion.py
+++ algorithm/Hackerrank_practice/timeConversion.py
@@ -1,2 +1,17 @@
 def timeConversion(s):
+    hh = s[:2]
+    period = s[-2:]
+
+    if (period == 'AM'):
+        if (hh == '12'):
+            output = '00' + s[2:-2]
+        else:
+            output = s[:-2]
     
+    elif (period == 'PM'):
+        if (hh == '12'):
+            output = s[0:-2]
+        else:
+            output = str(int(hh) + 12) + s[2:-2]
+
+    return output
