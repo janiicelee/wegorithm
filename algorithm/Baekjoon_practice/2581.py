d8eecd92e8c8a156b05dc6f6a73b1d27ac6d7bb1
Yerin Lee <meekukin@gmail.com>
2020-05-02 20:00:16 +0900

    boj wego

--- algorithm/Baekjoon_practice/2581.py
+++ algorithm/Baekjoon_practice/2581.py
@@ -1,19 +1,17 @@
-sosu = [0 for i in range(10001)]
-sosu[1] = 1
-for i in range(2, 98):
-    for j in range(i * 2, 10001, i):
-        sosu[j] = 1
-m = int(input())
-n = int(input())
-sum = 0
-min = 0
-for i in range(n, m - 1, -1):
-    if sosu[i] == 0:
-        sum += i
-        min = i
-if sum == 0:
+M = int(input())
+N = int(input())
+prime = []
+for i in range(M, N+1):
+    if i != 1:
+        check = True
+        for j in range(2, i):
+            if i % j == 0:
+                check = False
+                break
+        if check:
+            prime.append(i)
+if len(prime) == 0:
     print(-1)
 else:
-    print(sum, min, sep='\n')
-
-# 출처: https://pacific-ocean.tistory.com/128?category=810810
+    print(sum(prime))
+    print(prime[0])
